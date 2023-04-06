c9f1b989093db1291590c516db3335a47c63482b
NamsYes <102303373+NamsYes@users.noreply.github.com>
2023-04-06 12:15:29 +0200

    Ajout Route Dash

--- parking-app/resources/views/layouts/navigation.blade.php
+++ parking-app/resources/views/layouts/navigation.blade.php
@@ -72,7 +72,7 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('home')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                {{ __('dashboard') }}
             </x-responsive-nav-link>
         </div>
 
